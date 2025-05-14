02a8232c29a0451282084ad3ca7d90a179576e25
Roland Metivier <metivier.roland@chlorophyt.us>
2025-05-14 14:47:20 -0500

    error HTML needs layout

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ config :exfwghtblog, ExfwghtblogWeb.Endpoint,
   adapter: Bandit.PhoenixAdapter,
   render_errors: [
     formats: [html: ExfwghtblogWeb.ErrorHTML, json: ExfwghtblogWeb.ErrorJSON],
-    layout: false
+    layout: true
   ],
   pubsub_server: Exfwghtblog.PubSub,
   live_view: [signing_salt: "IdE0CcmQ"]
